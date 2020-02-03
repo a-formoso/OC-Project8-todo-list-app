15c9e0964ab42eb9b1fada663262a0f0bc208f77
Alexandre <alexandre_formoso@hotmail.com>
2020-02-03 21:57:47 +0000

    Add modal images to documentation

--- documentation/js/doc.js
+++ documentation/js/doc.js
@@ -1,3 +1,4 @@
+// DINAMIC NAVIGATION TABS
 
 function openContent(event, tabName) {
   // Declaring all variables
@@ -22,3 +23,29 @@ function openContent(event, tabName) {
 
 // select a tab to be open by default
 document.getElementById('openByDefault').click();
+
+
+
+//MODAL IMAGES
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = $('.scaleImg');
+var modalImg = $("#img01"); // jQuery stores collection of IDs
+var captionText = document.getElementById("caption");
+$('.scaleImg').click(function(){
+    modal.style.display = "block";
+    var newSrc = this.src;
+    modalImg.attr('src', newSrc);
+    captionText.innerHTML = this.alt;
+});
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
